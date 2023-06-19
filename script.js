3a32d5bf171f78ad5395972a5d984ce4d1ec95b2
exit <halimshams8@gmail.com>
2023-06-19 19:05:27 +0430

    UI improved

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const playerTwoScoreHTML = document.querySelector('#playerTwoScore');
 const playerTwoCurrentScoreHTML = document.querySelector(
 	'#playerTwoCurrentScore'
 );
+const diceImg = document.querySelector('#diceImg');
 
 // Buttons
 const newGameBtn = document.querySelector('#newGame');
@@ -14,8 +15,11 @@ const holdBtn = document.querySelector('#hold');
 
 // Code/Program
 rollDiceBtn.addEventListener('click', () => {
-	const randomDiceNumber = Math.trunc(Math.random() * 7);
+	const randomDiceNumber = Math.trunc(Math.random() * 6) + 1;
 	console.log(randomDiceNumber);
+	diceImg.src = `assets/dice${randomDiceNumber}.png`;
+
+	// Condition
 	if (randomDiceNumber == 1) {
 		console.log('User Switched!');
 	} else {
